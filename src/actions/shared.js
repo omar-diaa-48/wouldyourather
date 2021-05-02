7b51e342b46a06a91d5601093a5edd30a5774d1e
Omar Diaaeldine Elwakeel <67291253+omar-diaa-48@users.noreply.github.com>
2021-05-02 23:44:36 +0200

    logo added

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,10 +1,8 @@
-import { setAuthedUser } from './authedUser';
 import { receiveQuestions } from "./questions";
 import { receiveUsers } from "./users";
 import { _getUsers,_getQuestions } from '../utils/_DATA';
 import { showLoading, hideLoading } from "react-redux-loading";
 
-const AUTHED_USER_ID = 'johndoe'
 
 export function handleInitialData() {
     return (dispatch) => {
@@ -13,7 +11,6 @@ export function handleInitialData() {
                     .then(([users, questions]) => {
                         dispatch(receiveUsers(users))
                         dispatch(receiveQuestions(questions))
-                        dispatch(setAuthedUser(AUTHED_USER_ID))
                         dispatch(hideLoading())
                     })
     }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import { Redirect } from 'react-router';
+import { withRouter } from 'react-router';
 import { setAuthedUser } from '../actions/authedUser';
+import logo from '../visual/logo.png';
 
 const Login = (props) => {
     const handleUserChange =(e) => {
         e.preventDefault();
-        const {dispatch} = props
+        const {dispatch, history} = props
         if(e.target.selectedIndex !== 0){
             dispatch(setAuthedUser(e.target.value))
-            // return <Redirect to={`/user/${e.target.value}`} />
+            history.push('/')
         }
     }
     return(
@@ -17,10 +18,16 @@ const Login = (props) => {
             <div className='w-75 mx-auto mt-5 text-center'>
                 <span className='display-5'>Welcome to 'Would you rather?' app</span>
                 <p>Please sign in to continue</p>
+                <img className='logo'    
+                     alt='logo'
+                     src={logo}>
+                </img>
                 <div className="form-group mt-5">
                     <select className="form-select" onChange={handleUserChange}>
                         <option defaultValue>Choose Profile...</option>
-                        {props.usersIds.map(id => <option key={id} value={id}>
+                        {props.usersIds.map(id => 
+                        <option key={id} 
+                                value={id}>
                             {id}
                         </option>)}        
                     </select>
@@ -33,8 +40,9 @@ const Login = (props) => {
 function mapStateToProps({users}) {
     const usersIds = Object.keys(users)
     return{
-        usersIds
+        usersIds,
+        users
     }
 }
 
-export default connect(mapStateToProps)(Login)
+export default withRouter(connect(mapStateToProps)(Login))
