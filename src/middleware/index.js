b6be1a9e3ceca5adbb7b67ee392a8023d823e190
Omar Diaaeldine Elwakeel <67291253+omar-diaa-48@users.noreply.github.com>
2021-05-03 22:53:33 +0200

    use redux chrome devtool instead of logger

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,8 +1,10 @@
 import thunk from 'redux-thunk';
-import { applyMiddleware } from "redux";
-import logger from './logger';
+import { applyMiddleware, compose } from "redux";
+//import logger from './logger';
 
-export default applyMiddleware(
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose 
+
+export default composeEnhancers(applyMiddleware(
     thunk,
-    logger
-)
+    //logger
+))
